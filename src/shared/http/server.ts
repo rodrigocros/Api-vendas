f45d4934f871bc3d2820393396dd2457fdcdb359
Rodrigo Rosa <rodrigorosa.dev@gmail.com>
2023-08-12 10:25:29 -0300

    feat: initial config

---
+++ src/shared/http/server.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+import express, { NextFunction, Response, Request } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error'
+    });
+});
+
+app.listen(3333, () => console.log('Server is running!'));
